beb55c20840d137eea9106bcba8a37d9d6be9d74
DisneyCoding <disneyfangaming@gmail.com>
2026-08-14 17:44:26 -0400

    Updated old to new experience levels

--- scripts/events.py
+++ scripts/events.py
@@ -1925,7 +1925,7 @@ def handle_apprentice_EX(cat):
         if cat.not_working() and int(random.random() * 3):
             return
 
-        if cat.experience > cat.experience_levels_range["learning"][1]:
+        if cat.experience > cat.experience_levels_range["beginner"][1]:
             return
 
         if cat.status.rank == CatRank.MEDICINE_APPRENTICE:
